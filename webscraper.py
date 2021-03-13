544b4f6db55633d14e8f9920eae032182da874d2
Vineet Sukhthanker <vineetsuk@gmail.com>
2021-03-13 23:27:11 +0530

    sleep time reduced

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-
+from time import sleep
 
 url = 'https://gaana.com/search/survival%20by%20eminem'
 browser = webdriver.Chrome()
 browser.get(url)
-sleep(20)
+sleep(12)
 song = browser.find_element_by_class_name('loaded')
 song.click()
